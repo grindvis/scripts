1cb2b8bff6c1534120a877ef813638948b6dbe87
grindvis <56531267+grindvis@users.noreply.github.com>
2023-05-22 15:00:30 +0200

    Update install_updates.sh

--- install_updates.sh
+++ install_updates.sh
@@ -2,15 +2,21 @@
 # The set -e option instructs bash to immediately exit if any command has a non-zero exit status.
 set -e
 
-echo "Automatic update start `date +%Y-%m-%d" "%H:%M:%S`"
+# Set the location where the logfiles will be saved. 
+logging="/var/log/apt/automaticupdates.log"
+
+# Set a prompt so the . 
+echo "******************************************************************************************"
+echo "Automatic update started at `date +%Y-%m-%d" "%H:%M:%S`" >> $logging
+echo "******************************************************************************************"
 
 # Refresh the instlled repositories
-apt update -q >> /var/log/apt/automaticupdates.log
+apt-get update -q >> $logging
 
 # Install updates (prompts will be answered with "yes")
-apt upgrade -q -y >> /var/log/apt/automaticupdates.log
+apt-get upgrade -q -y >> $logging
 
 # Remove packages that were automatically installed to satisfy dependencies for other packages and are now no longer needed as dependencies changed (prompts will be answered with "yes").
-apt autoremove -q -y >> /var/log/apt/automaticupdates.log
+apt-get autoremove -q -y >> $logging
 
 exit
