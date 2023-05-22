5b82aeedfbc31ca219395240bfed9a78b8d8e604
grindvis <56531267+grindvis@users.noreply.github.com>
2023-05-22 10:54:32 +0200

    Update install_updates.sh

--- install_updates.sh
+++ install_updates.sh
@@ -3,12 +3,12 @@
 set -e
 
 # Refresh the instlled repositories
-apt update -q
+apt update -q >> /var/log/apt/automaticupdates.log
 
 # Install updates (prompts will be answered with "yes")
-apt upgrade -q -y
+apt upgrade -q -y >> /var/log/apt/automaticupdates.log
 
 # Remove packages that were automatically installed to satisfy dependencies for other packages and are now no longer needed as dependencies changed (prompts will be answered with "yes").
-apt autoremove -q -y
+apt autoremove -q -y >> /var/log/apt/automaticupdates.log
 
 exit
